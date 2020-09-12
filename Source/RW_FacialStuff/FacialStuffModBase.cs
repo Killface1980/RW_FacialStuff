04a624411490fe965c3b6cb261e31d14c1a1a1f3
BulletDelivery <kes5219@gmail.com>
2020-09-12 17:12:34 -0400

    Use HugsLib

---
+++ Source/RW_FacialStuff/FacialStuffModBase.cs
@@ -0,0 +1,34 @@
+﻿using HugsLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FacialStuff
+{
+	class FacialStuffModBase : ModBase
+	{
+		public override string ModIdentifier
+		{
+			get
+			{
+				return "FacialStuff";
+			}
+		}
+
+		protected override bool HarmonyAutoPatch
+		{ 
+			get
+			{
+				return false;
+			}
+		}
+
+		public override void DefsLoaded()
+		{
+			base.DefsLoaded();
+		}
+	}
+}
