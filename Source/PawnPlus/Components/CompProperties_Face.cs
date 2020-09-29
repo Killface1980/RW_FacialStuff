b456f49dc17b75a835b32d84996520a1d75ab5ee
RocketDelivery <kes5219@gmail.com>
2020-09-29 17:31:10 -0400

    Remove unused properties

--- Source/PawnPlus/Components/CompProperties_Face.cs
+++ Source/PawnPlus/Components/CompProperties_Face.cs
@@ -1,28 +1,12 @@
-﻿// ReSharper disable UnassignedField.Global
-// ReSharper disable StyleCop.SA1307
-// ReSharper disable StyleCop.SA1401
-// ReSharper disable InconsistentNaming
-
-using PawnPlus.Defs;
+﻿using PawnPlus.Defs;
 using PawnPlus.Parts;
 using System.Collections.Generic;
-
 using Verse;
 
 namespace PawnPlus
 {
     public class CompProperties_Face : CompProperties
-    {
-        public bool canRotateHead;
-        
-        public bool hasBeard;
-        
-        public bool hasMouth;
-        
-        public bool hasWrinkles;
-        
-        public bool needsBlankHumanHead;
-                
+    {               
         public IHeadBehavior headBehavior;
                 
         public List<IPartBehavior> partBehaviors;
