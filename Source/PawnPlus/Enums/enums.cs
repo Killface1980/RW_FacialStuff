ecd689d7e5f493c73f45384c41fada5a28be7dd2
RocketDelivery <kes5219@gmail.com>
2020-09-29 02:37:04 -0400

    Remove unused enums and classes

--- Source/PawnPlus/Enums/enums.cs
+++ Source/PawnPlus/Enums/enums.cs
@@ -1,11 +1,5 @@
 ﻿namespace PawnPlus
 {
-	public enum MouthState
-	{ 
-        Mood = 0,
-        Crying = 1
-    }
-
     public static class StringsFS
     {
         public const string PathHumanlike = "Things/Pawn/Humanlike/";
@@ -20,40 +14,5 @@
         public PartStatus FootLeft;
 
         public PartStatus FootRight;
-    }
-
-    public enum HandsToDraw
-    {
-        Both = 0,
-        RightHand = 1,
-        LeftHand = 2
-    }
-    public enum FullHead : byte
-    {
-        MaleAverageNormal = 0,
-
-        MaleAveragePointy = 1,
-
-        MaleAverageWide = 2,
-
-        MaleNarrowNormal = 3,
-
-        MaleNarrowPointy = 4,
-
-        MaleNarrowWide = 5,
-
-        FemaleAverageNormal = 6,
-
-        FemaleAveragePointy = 7,
-
-        FemaleAverageWide = 8,
-
-        FemaleNarrowNormal = 9,
-
-        FemaleNarrowPointy = 10,
-
-        FemaleNarrowWide = 11,
-
-        Undefined = 12
     }
 }
