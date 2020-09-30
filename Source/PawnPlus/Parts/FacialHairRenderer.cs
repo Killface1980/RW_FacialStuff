50225ad44670ba6fb03ede249d3f2883c387b895
RocketDelivery <kes5219@gmail.com>
2020-09-29 20:49:22 -0400

    Add back drawing for brows

---
+++ Source/PawnPlus/Parts/FacialHairRenderer.cs
@@ -0,0 +1,29 @@
+﻿using PawnPlus.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace PawnPlus.Parts
+{
+	class FacialHairRenderer : SimplePartRenderer
+	{
+		public override void Initialize(
+			Pawn pawn,
+			BodyDef bodyDef,
+			BodyPartRecord bodyPartRecord,
+			string defaultTexPath,
+			Dictionary<string, string> namedTexPaths,
+			BodyPartSignals bodyPartSignals)
+		{
+			_graphic = GraphicDatabase.Get<Graphic_Multi>(
+				defaultTexPath,
+				Shaders.FacePart,
+				Vector3.one,
+				pawn.story.hairColor);
+		}
+	}
+}
---
+++ Source/PawnPlus/Parts/SimplePartRenderer.cs
@@ -0,0 +1,58 @@
+﻿using PawnPlus.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace PawnPlus.Parts
+{
+	public class SimplePartRenderer : IPartRenderer
+	{
+		protected Graphic _graphic;
+
+		public virtual void Initialize(
+			Pawn pawn, 
+			BodyDef bodyDef, 
+			BodyPartRecord bodyPartRecord, 
+			string defaultTexPath, 
+			Dictionary<string, string> namedTexPaths, 
+			BodyPartSignals bodyPartSignals)
+		{
+			_graphic = GraphicDatabase.Get<Graphic_Multi>(
+				defaultTexPath,
+				Shaders.FacePart);
+		}
+		
+		public void Update(PawnState pawnState, in BodyPartStatus partStatus, ref bool updatePortrait)
+		{
+			
+		}
+		
+		public void Render(Vector3 rootPos, Quaternion rootQuat, Rot4 rootRot4, Vector3 renderNodeOffset, Mesh renderNodeMesh, bool portrait)
+		{
+			if(_graphic == null)
+			{
+				return;
+			}
+			Material partMat = _graphic.MatAt(rootRot4);
+			if(partMat != null)
+			{
+				Vector3 offset = rootQuat * renderNodeOffset;
+				GenDraw.DrawMeshNowOrLater(
+						renderNodeMesh,
+						rootPos + offset,
+						rootQuat,
+						partMat,
+						portrait);
+			}
+		}
+		
+		public object Clone()
+		{
+			return MemberwiseClone();
+		}
+	}
+}
